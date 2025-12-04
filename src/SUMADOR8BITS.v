bd8c00b9c98ad4da61ac4a1ed49bf068c2f55ffd
AlejandroJuarezLora <102187408+AlejandroJuarezLora@users.noreply.github.com>
2025-12-03 21:10:54 -0600

    Add files via upload
    Subiendo el circuito

---
+++ src/SUMADOR8BITS.v
@@ -0,0 +1,20 @@
+
+module SUMADOR8BITS(
+    input wire clk,
+    input wire rst,
+    input wire enable, 
+    output reg [7:0] out, 
+    output reg cout
+);
+
+    always@(posedge clk or negedge rst) begin
+        if(!rst)
+        begin
+                out = 8'd0;
+                cout = 1'b0;
+        end
+        else if(enable)
+            {cout,out} = {cout,out} + 9'd1;
+    end
+
+endmodule
